9b723e0cd02a31e1ddaad5243e3de1484ea38cb8
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-06-09 19:09:35 +0200

    Premier league table finish

--- src/components/Cards/TeamCard/TeamCard.jsx
+++ src/components/Cards/TeamCard/TeamCard.jsx
@@ -1,11 +1,48 @@
+/* eslint-disable react/prop-types */
 // import teams from '../../../pages/Teams/Teams'
+import { useState, useEffect } from 'react'
 import '../../../common/teams.json'
 import './TeamCard.css'
 
 function TeamCard  (props)  {
+  const [isActive, setIsActive] = useState(false)
+  const [, setTeamDescription] = useState("")
+ 
+
+  useEffect(() => {
+    if (isActive) {
+      setTeamDescription(
+        "Ovaj tim je osnovan 1886. godine. Najtrofejniji je klub u engleskoj..."
+      );
+    } else {
+      setTeamDescription("");
+    }
+  }, [isActive]);
+  
   return(
-    <div className='teams-card'>
-     <h1>{props?.wins}lla</h1>
+<div
+      className={isActive ? `team-card` : `team-card`}
+      style={{ height: isActive ? "180px" : "120px" }}
+    >
+
+      <div className='stats'><h2>Pos:</h2> <h2>Club:</h2> <h2>W</h2> <h2>L</h2> <h2>D</h2> <h2>PTS:</h2> </div>  
+      <h2 className='h2' style={{marginLeft: "50px"}}>{props.position}</h2>
+      <h2 className="h2">{props.team_name}</h2>
+      <p className="p">{props.matches}</p>
+      <p className="p">{props.wins}</p>
+      <p className="p">{props.losses}</p>
+      <p className="p">{props.draws}</p>
+      <p className="p">{props.points}</p>
+      <p className="p">
+        <button onClick={() => setIsActive(!isActive)} className="show">
+          {isActive ? "Show less" : "Show More"}
+        </button>
+      </p>
+      {isActive ? (
+        <p className="ptext">
+           {props?.desc}
+        </p>
+      ) : null}
     </div>
   )
 }
--- src/pages/Teams/Teams.jsx
+++ src/pages/Teams/Teams.jsx
@@ -1,26 +1,27 @@
-import teams from "../../common/hotels.json";
-import TeamCard from '../../components/Cards/TeamCard/TeamCard'
-import './Teams.css'
+/* eslint-disable react/jsx-key */
+import teams from "../../common/teams.json";
+import TeamCard from "../../components/Cards/TeamCard/TeamCard";
+import "./Teams.css";
 
-const  Teams = () => {
-return(
-  <div className="teams">
-    
-  {teams.map((team) => (
-    <TeamCard
-    key
-    team_name= {team.team_name}
-    points={team.points}
-    matches_played={team.matches_played}
-    goals_scored={team.goals_scored}
-    goals_conceded={team.goals_conceded}
-    wins={team.wins}
-    draws={team.draws}
-    losses={team.losses}
-    />
-  ))}
-</div>
+const Teams = () => {
+  return (
+    <div className="teams">
+      {teams.map((team) => (
+        <TeamCard
+          position={team.position}
+          team_name={team.team_name}
+          points={team.points}
+          matches_played={team.matches_played}
+          goals_scored={team.goals_scored}
+          goals_conceded={team.goals_conceded}
+          wins={team.wins}
+          draws={team.draws}
+          losses={team.losses}
+          desc={team.desc}
+        />
+      ))}
+    </div>
   );
-} 
-  
-export default Teams
+};
+
+export default Teams;
