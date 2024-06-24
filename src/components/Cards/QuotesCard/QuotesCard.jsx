5724f52fcfd50fd549f7908093a62743bf3e3ded
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-06-24 20:49:27 +0200

    Pagination for Quotes

--- src/components/Cards/QuotesCard/QuotesCard.jsx
+++ src/components/Cards/QuotesCard/QuotesCard.jsx
@@ -1,113 +1,31 @@
-/* eslint-disable no-unused-vars */
-// eslint-disable-next-line no-unused-vars
-import './QuotesCard.css'
-import '../../../common/quotes.json'
-import  '../../../pages/Quotes/Quotes';
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
-import * as React from 'react';
-import CardContent from '@mui/material/CardContent';
-
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import { FaHeart } from 'react-icons/fa';
-
-
-const ExpandMore  = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
- // eslint-disable-next-line react/prop-types
- const QuotesCard = ({ image,date,text,author,description}) => {
-
-  const [isActive, setIsActive] = React.useState(false)
-  const [input, setInput] = React.useState(1)
-  const [expanded, setExpanded] = React.useState(false);
-
-  const colorHeart = () => {
-    setIsActive(!isActive)
-   }
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
+function QuoteCard({ author, quote, tags }) {
   return (
-    <div className="QuotesCard">
-    <Card sx={{marginLeft:"20px",  maxWidth: 345 , maxHeight:"40rem"}}>
-      <CardHeader
-        avatar={
-          <Avatar  aria-label="recipe">
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={author}
-        subheader={date}
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image={image}
-        alt="Paella dish"
-      />
+    <Card sx={{ width: 275, boxShadow: "0px 10px 20px 5px black" }}>
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          {text}
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {author}
         </Typography>
+        <Typography variant="body3">{quote}</Typography>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          {tags.map((tag, index, arr) => {
+            const doesNextTagExists = arr[index + 1];
+            return (
+              <Typography sx={{ mb: 1.5 }} key={index}>
+                {tag}
+                {doesNextTagExists && ",  "}
+              </Typography>
+            );
+          })}
+        </div>
       </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-        <FaHeart onClick={colorHeart} className={isActive ? "heartClick" : "heart"} />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Description:</Typography>
-          <Typography paragraph>
-            {author}
-          </Typography>
-
-          <Typography paragraph>
-            {description} 
-          </Typography>
-        </CardContent>
-      </Collapse>
     </Card>
-    </div>
   );
 }
 
-export default QuotesCard
+export default QuoteCard;
--- src/pages/Quotes/Quotes.jsx
+++ src/pages/Quotes/Quotes.jsx
@@ -1,70 +1,74 @@
 import {  useState } from "react";
-import PaginationQuotes from '../../components/PaginationQuotes/PaginationQuotes'
-import quotes from "../../common/quotes.json";
 import QuotesCard from "../../components/Cards/QuotesCard/QuotesCard";
 import './Quotes.css'
 import { useEffect } from "react";
+import { Box, CircularProgress, Pagination, Stack } from "@mui/material";
 // import CircularProgress from '@mui/material/CircularProgress';
 // import Box from '@mui/material/Box';
 
 const Quotes = () => {
-  const [page, setPage] = useState(1)
+  const [quotes, setQuotes] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [page, setPage] = useState(1);
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+  const [totalPages, setTotalPages] = useState(0);
 
-  const numOfQuotesPerPage = 4
-  const numOfQuotes = quotes.length
-  const numOfPages = Math.ceil(numOfQuotes / numOfQuotesPerPage)
+
+
+  async function getQuotes  ()  {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `https://api.quotable.io/quotes?limit=16&page=${page}&sortBy=author&order=asc`
+      );
+      const result = await response.json();
+      setQuotes(result.results);
+      setTotalPages(result.totalPages);
+    } catch(error){
+      console.log(error)
+    } finally{
+      setIsLoading(false)
+    }
+  }
 
   useEffect(() => {
     window.scrollTo({
-      top: 40,
-      behavior:"smooth"
-    })
-  }, [page])
-  
-  // const [quotes, setQuotes] = useState([])
-  // const [isLoading, setIsLoading] = useState(true)
-
-  // async function getQuotes  ()  {
-  //    setIsLoading(true)
-  //
-  //   try{
-  //     const response = await fetch('https://api.quotable.io');
-  //     const result = await response.json()
-  //     setQuotes(result);
-  //     console.log(result)
-  //   } catch(error){
-  //     console.log(error)
-  //   } finally{
-  //     setIsLoading(false)
-  //   }
-  // }
-
-  // useEffect( () => {
-  //   getQuotes();
-  // }, [])
-
+      top: 0,
+      behavior: "smooth",
+    });
+    getQuotes();
+  }, [page]);
   return(
     <>
-    <div className="Quotes">
-       {/* {isLoading ?  <Box  sx={{ display: 'flex' }}>
-          <CircularProgress  />
-       </Box>  : } */}
-
-      {quotes.map((quote) => ( 
-       <QuotesCard 
-       key={quote.id}
-       author={quote.author}
-       text={quote.text}
-       date={quote.date}
-       image={quote.image}
-       description={quote.description}
-       author_image={quote.author_image}
-       />
-      ))
-      
-      .slice(numOfQuotesPerPage * (page - 1), numOfQuotesPerPage * page)}
+    <div className="quotes">
+      {isLoading ? (
+        <Box sx={{ display: "flex", position: "absolute", top: 200 }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        quotes.map((quote) => (
+          <QuotesCard
+            key={quote._id}
+            author={quote.author}
+            quote={quote.content}
+            tags={quote.tags}
+          />
+        ))
+      )}
     </div>
-    <PaginationQuotes page={page} numOfPages={numOfPages} setPage={setPage}/>
+    <div className="pagination-container">
+    <Stack spacing={2}>
+      <Pagination 
+              count={totalPages}
+              page={page}
+              onChange={handleChange}
+              color="primary" 
+      />
+
+    </Stack>
+    </div>
     </>
   )
 }
