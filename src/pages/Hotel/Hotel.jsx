6eceb2d18cc6ca11f51da422ab41b93414303aa5
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-06-27 18:26:03 +0200

    Local Storage set onClick button Hearth

--- src/pages/Hotel/Hotel.jsx
+++ src/pages/Hotel/Hotel.jsx
@@ -2,27 +2,49 @@ import { useParams } from "react-router-dom";
 import "./Hotel.css";
 import hotels from "../../common/hotels.json";
 // import { CiHeart,CiLock,CiParking1 } from "react-icons/ci";
-import {  FaWifi, FaSwimmer,FaSpa, FaCity, FaStreetView, FaFlag, FaParking, FaPray, FaHeart } from "react-icons/fa";
+import {  FaWifi, FaSwimmer,FaSpa, FaCity, FaStreetView, FaFlag, FaParking, FaPray, FaHeart, FaRegHeart } from "react-icons/fa";
 import { useState } from "react";
 import { useEffect } from "react";
 
 function Hotel() {
-  const [isActive, setIsActive] = useState(false)
-  const [input, setInput] = useState(1)
+  // const [isActive, setIsActive] = useState(false)
   const { id } = useParams();
-  const hotel = hotels.find((hotel) => hotel.id === +id)
-   
-  const colorHeart = () => {
-   setIsActive(!isActive)
-  }
+  const hotel = hotels.find((hotel) => hotel.id === +id);
+
+  const [input, setInput] = useState(1);
+  const [updatedFavoriteHotels, setUpdatedFavoriteHotels] = useState(() => {
+    const storedFavorites = localStorage.getItem("favoriteHotels");
+    return storedFavorites ? JSON.parse(storedFavorites) : [];
+  });
+
+  const [isFavoriteState, setIsFavoriteState] = useState(() => {
+    return updatedFavoriteHotels.includes(hotel.id);
+  });
+
+  // const colorHeart = () => {
+  //  setIsActive(!isActive)
+  // }
   const handleInputChanges = (e) => {
     setInput(e.target.value);
  };
-
- useEffect(() => {
-   localStorage.setItem (`Hotel sa id:${id}`, JSON.stringify({HotelName: hotel?.title}))
- }, [isActive] )
  
+ useEffect(() => {
+  localStorage.setItem("favoriteHotels", JSON.stringify(updatedFavoriteHotels));
+}, [updatedFavoriteHotels]);
+
+const handleFavoriteToggle = () => {
+  if (!hotel) return; // Ensure hotel is defined before proceeding
+  if (!isFavoriteState) {
+    const newFavoriteHotels = [...updatedFavoriteHotels, hotel.id];
+    setUpdatedFavoriteHotels(newFavoriteHotels);
+    setIsFavoriteState(true);
+  } else {
+    const newFavoriteHotels = updatedFavoriteHotels.filter((hotelId) => hotelId !== hotel.id);
+    setUpdatedFavoriteHotels(newFavoriteHotels);
+    setIsFavoriteState(false);
+  }
+};
+
   return(
   <div className="hotel">
     <div className="card-hotel">
@@ -42,7 +64,13 @@ function Hotel() {
             <button><FaSpa /></button>
             <button><FaParking /></button>
             <button><FaPray /></button>
-            <button onClick={colorHeart} className={isActive ? "heartClick" : "heart"}><FaHeart /></button>
+            <button onClick={handleFavoriteToggle}>
+               {isFavoriteState ? (
+                  <FaHeart fill="red" className="heart"  style={{cursor:"pointer"}} />
+                ) : (
+                  <FaRegHeart fill="red" className="heartClick" style={{cursor:"pointer"}} />
+                )}
+            </button>
          </div>
 
          <div className="price-hotel">
--- src/pages/Hotels/Hotels.jsx
+++ src/pages/Hotels/Hotels.jsx
@@ -4,15 +4,17 @@ import "./Hotels.css";
 import Pagination from "../../components/Paginations/Pagination";
 import { useState } from "react";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function HotelsList() {
-
+  const navigate = useNavigate();
   const [page, setPage] = useState(1)
   const numOfHotelPerPage = 8
   //Nums of hotels
   const numOfHotels = hotels.length
   //Num of Pages
   const numOfPages = Math.ceil(numOfHotels / numOfHotelPerPage)
+  const [favoriteHotels, setFavoriteHotels] = useState([]);
 
   useEffect(() => {
    window.scrollTo({
@@ -20,24 +22,45 @@ function HotelsList() {
     behavior:"smooth"
   })
   }, [page])
+
+  useEffect(() => {
+    try {
+      const storedHotels = localStorage.getItem('favoriteHotels');
+      if (storedHotels) {
+        setFavoriteHotels(JSON.parse(storedHotels));
+      }
+    } catch (error) {
+      console.error('Failed to parse favoriteHotels from localStorage:', error);
+    }
+  }, []);
   return (
     <>
     <div className="hotels">
-      {hotels.map((hotel) => (
-        <HotelCard
-          key={hotel.id}
-          imageUrl={hotel.imageUrl}
-          title={hotel.title}
-          content={hotel.content}
-          stars={hotel.stars}
-          total={hotel.total}
-          city={hotel.city}
-          state={hotel.state}
-          country={hotel.country}
-          rates_from={hotel.rates_from}
-          onClick={() => (window.location.href = `/hotels/${hotel.id}`)}
-        />
-      ))
+      {hotels.map((hotel) => {
+            const isFavorite = favoriteHotels.includes(hotel.id);
+            return (
+              <HotelCard
+                key={hotel.id}
+                imageUrl={hotel.imageUrl}
+                title={hotel.title}
+                content={hotel.content}
+                total={hotel.total}
+                stars={hotel.stars}
+                city={hotel.city}
+                state={hotel.state}
+                country={hotel.country}
+                rates_from={hotel.rates_from}
+                onClick={() =>
+                  navigate(`/hotels/${hotel.id}`, {
+                    state: {
+                      isFavorite: isFavorite,
+                      favoriteHotels: favoriteHotels,
+                    },
+                  })
+                }
+              />
+            );
+          })
       // .slice(0, numOfHotelPerPage) //Prikaz prvih 8 hotela
       // .slice(numOfHotelPerPage, numOfHotels ) //Prikaz drugih 8
       // .slice(16, 24)
