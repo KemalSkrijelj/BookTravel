7b5eb71866b4d94d3a0691080991e4974b781b58
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-07-05 20:20:41 +0200

    Add 404-notFound page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 // import MySentence from "./components/MySentence/MySentence";
 // import Greeting from "./components/Greeting/Greeting";
 import { Navbar } from "./components/Navbar/Navbar";
-import Footer  from "./components/Footer/Footer";
+import Footer from "./components/Footer/Footer";
 import { Route, Routes, useNavigate } from "react-router-dom";
 
 import HotelsList from "./pages/Hotels/Hotels";
@@ -14,7 +14,8 @@ import FavList from "./pages/YourList/YourList";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 import Quotes from "./pages/Quotes/Quotes";
-import Auth from './pages/Auth/Auth'
+import Auth from "./pages/Auth/Auth";
+import NotFound from "./pages/NotFound/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 
 import { Home } from "./pages/Home/Home";
@@ -23,7 +24,7 @@ import { AppContext } from "./context/AppContext";
 
 function App() {
   const { setLoggedInUser } = useContext(AppContext);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
     const localLoggedInUser = localStorage.getItem("loggedInUser");
@@ -38,48 +39,95 @@ function App() {
   return (
     <div className="app">
       <Navbar />
-          <Routes>
-            <Route path="/auth" element={!localLoggedInUser ? <Auth /> : <Home/> } />
-            {/* <Route path="/auth" element={!localLoggedInUser ? (<Route path="/auth" element={<Auth />} />) : (navigate("/"))} /> */}
+      <Routes>
+        <Route
+          path="/auth"
+          element={!localLoggedInUser ? <Auth /> : <Home />}
+        />
+        {/* <Route path="/auth" element={!localLoggedInUser ? (<Route path="/auth" element={<Auth />} />) : (navigate("/"))} /> */}
 
-            <Route path="/"element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>} />
-            
-            <Route path="/hotels-list" element={
-              <ProtectedRoute>
-                <HotelsList/>
-              </ProtectedRoute>} />
-            <Route path="/hotels/:id" element={
-              <ProtectedRoute>
-                <Hotel/>
-              </ProtectedRoute>} />
-            <Route path="/yourList"  element={
-              <ProtectedRoute>
-                <FavList />
-              </ProtectedRoute>}/>
-            <Route path="/about-us" element={
-              <ProtectedRoute>
-                <AboutUs />
-              </ProtectedRoute>}/>
-            <Route path="/Login" element={
-              <ProtectedRoute>
-                <Login />
-              </ProtectedRoute>} />
-            <Route path="/signUp" element={
-              <ProtectedRoute>
-                <SignUp />
-              </ProtectedRoute>} />
-            <Route path="/quotes"  element={
-              <ProtectedRoute>
-                <Quotes />
-              </ProtectedRoute> }/>
-            <Route path='/teams' element={
-              <ProtectedRoute>
-                <Teams />
-              </ProtectedRoute>}/>
-          </Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/hotels-list"
+          element={
+            <ProtectedRoute>
+              <HotelsList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/hotels/:id"
+          element={
+            <ProtectedRoute>
+              <Hotel />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/yourList"
+          element={
+            <ProtectedRoute>
+              <FavList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/about-us"
+          element={
+            <ProtectedRoute>
+              <AboutUs />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/Login"
+          element={
+            <ProtectedRoute>
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/signUp"
+          element={
+            <ProtectedRoute>
+              <SignUp />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/quotes"
+          element={
+            <ProtectedRoute>
+              <Quotes />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/teams"
+          element={
+            <ProtectedRoute>
+              <Teams />
+            </ProtectedRoute>
+          }
+        />
+                <Route
+          path="*"
+          element={
+            <ProtectedRoute>
+              <NotFound />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
       <Footer />
     </div>
   );
